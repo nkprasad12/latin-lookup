2891c3c23e34fb1988453e09db032f20f7131e13
Nitin Prasad <nkprasa@gmail.com>
2021-12-23 18:06:32 -0600

    Add basic functionality, strip macra and open Wiki

--- background.js
+++ background.js
@@ -1,6 +1,23 @@
-let color = '#3aa757';
+const MENU_ITEM_ID = "LatinWikiLookupMenuItem"
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  chrome.contextMenus.create({
+    "id": MENU_ITEM_ID,
+    "title": "Look up on Wiktionary",
+    "contexts": ["selection"],
+  });
 });
+
+chrome.contextMenus.onClicked.addListener(
+    (info, tab) => {
+        if (info.menuItemId !== MENU_ITEM_ID) {
+            return
+        }
+        const input = info.selectionText
+        console.log('Input: ' + input)
+        const fixedInput = input.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+        console.log('Fixed Input: ' + fixedInput)
+        const newURL = "https://en.wiktionary.org/wiki/" + fixedInput + "#Latin";
+        chrome.tabs.create({ url: newURL });
+    } 
+)
